f1473b644671c566fff6f915f6be6290e0f211a9
vyshnavikandula <vyshnavikandula02@gmail.com>
2026-06-25 23:03:05 +0530

    Create J_Multiples.cpp

---
+++ Week1/J_Multiples.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    long long A, B;
+    cin >> A >> B;
+
+    if (A % B == 0 || B % A == 0)
+        cout << "Multiples";
+    else
+        cout << "No Multiples";
+
+    return 0;
+}
